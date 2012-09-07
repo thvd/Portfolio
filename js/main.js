8d94faa929754a8b67abb305de8b380bb5c24ac1
Theodoor van Donge <theodoorvd@gmail.com>
2012-09-07 19:43:52 +0200

    Initial commit, of my portfolio.

---
+++ js/main.js
@@ -0,0 +1,31 @@
+(function($) {
+	$(document).on('ready', function() {
+		var anchor = $('header').find('figure').find('a');
+		var map = anchor.find('img');
+		var $window = $(window);
+
+		$window.on('resize', function() {
+			var height = 300;
+			var width = $window.width();
+			width -= (width / 100) * 12;
+			width = parseInt(width, 10);
+			
+			anchor.css({
+				backgroundSize : '100%',
+				opacity: .5
+			});
+
+			map.one('load', function() {
+				anchor.css({
+					backgroundSize: '200%',
+					opacity: 1
+				});
+			});	
+
+			var url = 'http://maps.googleapis.com/maps/api/staticmap?center=Barneveld&zoom=10&size=' + width * 2 + 'x' + height * 2 + '&key=AIzaSyADpc4Hqul9KkwAhVtj8RbN-t14J6TOhp0&sensor=false&markers=Barneveld&scale=2';
+			map.attr('src', url);
+			anchor.css('background-image', 'url(' + url + ')');
+
+		}).trigger('resize');
+	});
+}) (jQuery);
